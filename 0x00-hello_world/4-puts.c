358c35aa56bf58a59a2ef5095107c8020029dccb
kz <zsemrnael@gmail.com>
2022-12-08 13:49:04 -0800

    print something

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - main block
- * return: 0
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
