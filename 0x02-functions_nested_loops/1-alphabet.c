bae1539e36833cf4834dfd3b1e1a2790cfd07802
kz <zsemrnael@gmail.com>
2022-12-13 13:35:54 -0800

    lowercase-alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * lowercase alphabet - print lowercase alphabet
+ */
+
+void print_alphabet(void);
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+	_putchar('\n');
+}
