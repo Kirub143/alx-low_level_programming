63c6e8527f2bfa293b1fa12fd27ee0769ea58835
kz <zsemrnael@gmail.com>
2022-12-13 13:55:06 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * main - prints _putchar
+ * main - print putchar
  *
- * Return: Always 0 (success)
+ * Return: Always 0
  */
 
 int main(void)
 {
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
