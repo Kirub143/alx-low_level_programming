4a899d540697a04e64f73e5e56ac9bd7de4ae82f
kz <zsemrnael@gmail.com>
2022-12-19 15:00:57 -0800

    i am almost on the peak of high hill to lost hope

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * puts_half - prints half of a string,
@@ -21,15 +22,15 @@ void puts_half(char *str)
 	{
 		for (i = len / 2; str[i] != '\0'; i++)
 		{
-			_putchar(i);
+			putchar(i);
 		}
 	}
 	else if (len % 2)
 	{
 		for (n = (len - 1) / 2; str[i] != '\0'; i++)
 		{
-			_putchar(str[n + 1]);
+			putchar(str[n + 1]);
 		}
 	}
-	_putchar('\n');
+	putchar('\n');
 }
