0d57c14315b9494a325108d9a93a46136a33a6b5
kz <zsemrnael@gmail.com>
2022-12-20 12:09:16 -0800

    mm

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,1 @@
-#include "main.h"
-
-/**
- * _puts - prints a string.
- * @str: pointer to the string to pint
- * Return: void
- */
-
-void _puts(char *str)
-{
-	while (*str != '\0')
-	{
-		_putchar(*str++);
-	}
-		_putchar('\n');
 }
