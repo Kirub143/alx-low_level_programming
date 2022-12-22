94fb6c67e394c0e6449d24f91cb0a96b7275c6f9
kz <zsemrnael@gmail.com>
2022-12-22 12:28:45 -0800

    capitalization

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -29,10 +29,8 @@ char *cap_string(char *str)
 			str[index - 1] == '{' ||
 			str[index - 1] == '}' ||
 			index == 0)
-				str[index] -= 32;
-
+			str[index] -= 32;
 		index++;
 	}
-
 	return (str);
 }
