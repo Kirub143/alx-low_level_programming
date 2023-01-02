1f36cbb63af77871b957bc8c0656a5f7ad7881e1
kz <zsemrnael@gmail.com>
2023-01-02 09:45:20 -0800

    locate a specific character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: input
+ * @c: input
+ * Return: Always 0 (succces)
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
