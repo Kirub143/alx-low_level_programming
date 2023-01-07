32d33b24a4dce120367743d3a756aa289c3273ac
kz <zsemrnael@gmail.com>
2023-01-06 16:05:22 -0800

    the static library

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - locates substring
+ * @haystack: container
+ * @needle: string to be searched
+ *
+ * Return: Always 0  (success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l =  haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
+}
