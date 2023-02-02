c15e0bc186d92a71aeae32bf51ed6f46c1f74838
kz <zsemrnael@gmail.com>
2023-02-02 14:21:03 -0800

    binary to uint

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -10,6 +7,3 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int _atoi(const char *s);
 int _putchar(char c);
 int get_endianness(void);
-
-#endif
-
